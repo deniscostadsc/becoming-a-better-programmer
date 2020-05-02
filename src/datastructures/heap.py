d067770b45bc01d13e0a26715ad6563e874c86cd
Denis Costa <deniscostadsc@gmail.com>
2020-05-02 15:38:44 -0300

    Split generic thigs into heap class

--- src/datastructures/heap.py
+++ src/datastructures/heap.py
@@ -5,102 +5,105 @@ class EmptyHeap(Exception):
     pass
 
 
-class MinHeap:
+class Heap():
     def __init__(self) -> None:
-        self.__heap: List[int] = []
+        self._heap: List[int] = []
 
     def __len__(self) -> int:
-        return len(self.__heap)
+        return len(self._heap)
 
     def __getitem__(self, index: int) -> int:
-        return self.__heap[index]
+        return self._heap[index]
 
-    def __has_parent(self, index: int) -> bool:
-        return self.__parent_index(index) >= 0
+    def _has_parent(self, index: int) -> bool:
+        return self._parent_index(index) >= 0
 
-    def __has_left_child(self, index: int) -> bool:
-        return self.__left_child_index(index) < len(self)
+    def _has_left_child(self, index: int) -> bool:
+        return self._left_child_index(index) < len(self)
 
-    def __has_right_child(self, index: int) -> bool:
-        return self.__right_child_index(index) < len(self)
+    def _has_right_child(self, index: int) -> bool:
+        return self._right_child_index(index) < len(self)
 
-    def __parent(self, index: int) -> int:
-        return self.__heap[self.__parent_index(index)]
+    def _parent(self, index: int) -> int:
+        return self._heap[self._parent_index(index)]
 
-    def __left_child(self, index: int) -> int:
-        return self.__heap[self.__left_child_index(index)]
+    def _left_child(self, index: int) -> int:
+        return self._heap[self._left_child_index(index)]
 
-    def __right_child(self, index: int) -> int:
-        return self.__heap[self.__right_child_index(index)]
+    def _right_child(self, index: int) -> int:
+        return self._heap[self._right_child_index(index)]
 
-    def __parent_index(self, index: int) -> int:
+    def _parent_index(self, index: int) -> int:
         return int((index - 1) / 2)
 
-    def __left_child_index(self, parent_index: int) -> int:
+    def _left_child_index(self, parent_index: int) -> int:
         return parent_index * 2 + 1
 
-    def __right_child_index(self, parent_index: int) -> int:
+    def _right_child_index(self, parent_index: int) -> int:
         return parent_index * 2 + 2
 
-    def __swap(self, first_index: int, second_index: int) -> None:
-        self.__heap[first_index], self.__heap[second_index] = (
-            self.__heap[second_index],
-            self.__heap[first_index],
+    def _swap(self, first_index: int, second_index: int) -> None:
+        self._heap[first_index], self._heap[second_index] = (
+            self._heap[second_index],
+            self._heap[first_index],
         )
 
-    def __heapfy_up(self) -> None:
-        index = len(self) - 1
-        value = self.__heap[index]
-
-        while self.__has_parent(index) and self.__parent(index) > value:
-            self.__swap(index, self.__parent_index(index))
-            index = self.__parent_index(index)
-            value = self.__heap[index]
-
-    def __min_child_index(self, index: int) -> int:
-        min_child_index = self.__left_child_index(index)
-        if self.__has_right_child(index):
-            if self.__right_child(index) < self.__left_child(index):
-                min_child_index = self.__right_child_index(index)
-        return min_child_index
-
-    def __heapfy_down(self) -> None:
-        index = 0
-        value = self.__heap[index]
-
-        while self.__has_left_child(index):
-            min_child_index = self.__min_child_index(index)
-
-            if value < self.__heap[min_child_index]:
-                break
-            else:
-                self.__swap(index, min_child_index)
-
-            index = min_child_index
-            value = self.__heap[index]
-
     def push(self, value: int) -> None:
-        self.__heap.append(value)
-        self.__heapfy_up()
+        self._heap.append(value)
+        self._heapfy_up()
 
     def peek(self) -> int:
         if not len(self):
             raise EmptyHeap("you cannot peek on empty heap")
 
-        return self.__heap[0]
+        return self._heap[0]
 
     def pop(self) -> int:
         if not len(self):
             raise EmptyHeap("you cannot pop on empty heap")
 
-        pop = self.__heap[0]
+        pop = self._heap[0]
         if len(self) == 1:
-            del self.__heap[0]
+            del self._heap[0]
         else:
-            self.__heap[0] = self.__heap[-1]
-            del self.__heap[-1]
-            self.__heapfy_down()
+            self._heap[0] = self._heap[-1]
+            del self._heap[-1]
+            self._heapfy_down()
+
         return pop
+
+
+class MinHeap(Heap):
+    def _heapfy_up(self) -> None:
+        index = len(self) - 1
+        value = self._heap[index]
+
+        while self._has_parent(index) and self._parent(index) > value:
+            self._swap(index, self._parent_index(index))
+            index = self._parent_index(index)
+            value = self._heap[index]
+
+    def _min_child_index(self, index: int) -> int:
+        min_child_index = self._left_child_index(index)
+        if self._has_right_child(index):
+            if self._right_child(index) < self._left_child(index):
+                min_child_index = self._right_child_index(index)
+        return min_child_index
+
+    def _heapfy_down(self) -> None:
+        index = 0
+        value = self._heap[index]
+
+        while self._has_left_child(index):
+            min_child_index = self._min_child_index(index)
+
+            if value < self._heap[min_child_index]:
+                break
+            else:
+                self._swap(index, min_child_index)
+
+            index = min_child_index
+            value = self._heap[index]
 
 
 __all__ = ["EmptyHeap", "MinHeap"]
--- tests/datastructures/test_min_heap.py
+++ tests/datastructures/test_min_heap.py
@@ -11,75 +11,75 @@ def equal_items(expected, actual):
 
 
 @pytest.fixture
-def heap():
+def min_heap():
     return MinHeap()
 
 
-def test_heap_size_is_initially_zero(heap):
-    assert len(heap) == 0
+def test_min_heap_size_is_initially_zero(min_heap):
+    assert len(min_heap) == 0
 
 
-def test_push_item_to_heap(heap):
-    heap.push(1)
-    assert len(heap) == 1
+def test_push_item_to_min_heap(min_heap):
+    min_heap.push(1)
+    assert len(min_heap) == 1
 
 
-def test_push_item_that_should_be_replaced(heap):
-    heap.push(2)
-    heap.push(3)
-    heap.push(4)
-    heap.push(1)
-    assert heap.peek() == 1
-    assert pytest.helpers.equal_items([1, 2, 4, 3], heap)
+def test_push_item_that_should_be_replaced(min_heap):
+    min_heap.push(2)
+    min_heap.push(3)
+    min_heap.push(4)
+    min_heap.push(1)
+    assert min_heap.peek() == 1
+    assert pytest.helpers.equal_items([1, 2, 4, 3], min_heap)
 
 
-def test_heap_peek(heap):
-    heap.push(1)
-    assert heap.peek() == 1
-    assert pytest.helpers.equal_items([1], heap)
+def test_min_heap_peek(min_heap):
+    min_heap.push(1)
+    assert min_heap.peek() == 1
+    assert pytest.helpers.equal_items([1], min_heap)
 
 
-def test_peek_empty_heap(heap):
+def test_peek_empty_min_heap(min_heap):
     with pytest.raises(EmptyHeap) as e:
-        heap.peek()
+        min_heap.peek()
     assert str(e.value) == "you cannot peek on empty heap"
 
 
-def test_heap_pop(heap):
-    heap.push(3)
-    assert pytest.helpers.equal_items([3], heap)
-    assert len(heap) == 1
-    assert heap.pop() == 3
-    assert len(heap) == 0
-    assert pytest.helpers.equal_items([], heap)
+def test_min_heap_pop(min_heap):
+    min_heap.push(3)
+    assert pytest.helpers.equal_items([3], min_heap)
+    assert len(min_heap) == 1
+    assert min_heap.pop() == 3
+    assert len(min_heap) == 0
+    assert pytest.helpers.equal_items([], min_heap)
 
 
-def test_pop_empty_heap(heap):
+def test_pop_empty_min_heap(min_heap):
     with pytest.raises(EmptyHeap) as e:
-        heap.pop()
+        min_heap.pop()
     assert str(e.value) == "you cannot pop on empty heap"
 
 
-def test_pop_item_and_check_heap_reorganization(heap):
-    heap.push(10)
-    heap.push(20)
-    heap.push(15)
-    heap.push(17)
-    assert pytest.helpers.equal_items([10, 17, 15, 20], heap)
-    assert heap.pop() == 10
-    assert len(heap) == 3
-    assert heap.peek() == 15
-    assert pytest.helpers.equal_items([15, 17, 20], heap)
+def test_pop_item_and_check_min_heap_reorganization(min_heap):
+    min_heap.push(10)
+    min_heap.push(20)
+    min_heap.push(15)
+    min_heap.push(17)
+    assert pytest.helpers.equal_items([10, 17, 15, 20], min_heap)
+    assert min_heap.pop() == 10
+    assert len(min_heap) == 3
+    assert min_heap.peek() == 15
+    assert pytest.helpers.equal_items([15, 17, 20], min_heap)
 
 
-def test_pop_item_and_reorganize_and_should_stop_in_the_middle(heap):
-    heap.push(10)
-    heap.push(15)
-    heap.push(20)
-    heap.push(17)
-    heap.push(16)
-    assert pytest.helpers.equal_items([10, 15, 20, 17, 16], heap)
-    assert heap.pop() == 10
-    assert len(heap) == 4
-    assert heap.peek() == 15
-    assert pytest.helpers.equal_items([15, 16, 20, 17], heap)
+def test_pop_item_and_reorganize_and_should_stop_in_the_middle(min_heap):
+    min_heap.push(10)
+    min_heap.push(15)
+    min_heap.push(20)
+    min_heap.push(17)
+    min_heap.push(16)
+    assert pytest.helpers.equal_items([10, 15, 20, 17, 16], min_heap)
+    assert min_heap.pop() == 10
+    assert len(min_heap) == 4
+    assert min_heap.peek() == 15
+    assert pytest.helpers.equal_items([15, 16, 20, 17], min_heap)
