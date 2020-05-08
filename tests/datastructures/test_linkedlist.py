2641591e7dc362a5f968755befbe1da3834d7043
Denis Costa <deniscostadsc@gmail.com>
2020-05-08 08:09:32 -0300

    Assert all items on linked list

--- tests/datastructures/test_linkedlist.py
+++ tests/datastructures/test_linkedlist.py
@@ -25,6 +25,7 @@ def test_append_items_to_linked_list(linked_list):
     linked_list.append(3)
 
     assert len(linked_list) == 3
+    assert pytest.helpers.equal_items([1, 2, 3], linked_list)
 
 
 def test_prepend_items_to_linked_list(linked_list):
@@ -33,3 +34,4 @@ def test_prepend_items_to_linked_list(linked_list):
     linked_list.prepend(3)
 
     assert len(linked_list) == 3
+    assert pytest.helpers.equal_items([3, 2, 1], linked_list)
