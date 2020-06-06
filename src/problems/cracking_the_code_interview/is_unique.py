9138e62a594e1db641284c394c33194bae3c18ba
Denis Costa <deniscostadsc@gmail.com>
2020-06-06 20:10:08 -0300

    Solve is_unique problem

--- src/problems/cracking_the_code_interview/is_unique.py
+++ src/problems/cracking_the_code_interview/is_unique.py
@@ -1,7 +1,22 @@
+from typing import Dict
+
+
 def has_unique_char(s: str) -> bool:
-    if s == "aa":
-        return False
-    return True
+    """
+    Time: O(n)
+    Space: O(c)
+
+    "c" being the number of unique characters in the string
+    """
+    chars: Dict[str, int] = {}
+
+    for char in s:
+        if char in chars:
+            chars[char] += 1
+        else:
+            chars[char] = 1
+
+    return 1 in chars.values()
 
 
 __all__ = [
--- tests/problems/cracking_the_code_interview/test_is_unique.py
+++ tests/problems/cracking_the_code_interview/test_is_unique.py
@@ -7,3 +7,11 @@ def test_has_unique_char():
 
 def test_has_not_unique_char():
     assert not has_unique_char("aa")
+
+
+def test_has_unique_char_within():
+    assert has_unique_char("bbbcddd")
+
+
+def test_has_not_unique_char_within():
+    assert not has_unique_char("bbbddd")
