56a4f78f502cb123e2be8b8afaddddb714b7d16b
Denis Costa <deniscostadsc@gmail.com>
2020-06-07 13:01:19 -0300

    Add naive approach for search_in_adjacent_numbers

---
+++ src/problems/coder_career/search_in_adjacent_numbers.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+"""
+Given an array where two neighboring elements are adjacent (in absolute
+difference 1), can you write an algorithm to search an element in the array and
+return its position? If the element appears multiple times, please return the
+first occurrence.
+
+For example, if given the array {4, 5, 6, 5, 6, 7, 8, 9, 10, 9} and an element
+9, the element appears twice in the array, and the first occurrence is at
+position 7.
+"""
+
+
+def search_in_adjacent_numbers(numbers: List[int], search: int) -> int:
+    """
+    Time: O(n)
+    Space: O(1)
+    """
+    for index, number in enumerate(numbers):
+        if search == number:
+            return index
+
+    raise ValueError(f"{search} is not in list")
---
+++ tests/problems/coder_career/test_search_in_adjacent_numbers.py
@@ -0,0 +1,21 @@
+import pytest
+
+from problems.coder_career.search_in_adjacent_numbers import (
+    search_in_adjacent_numbers,
+)
+
+
+def test_search_in_adjacent_numbers():
+    assert search_in_adjacent_numbers([1, 2, 1], 1) == 0
+    assert search_in_adjacent_numbers([1, 2, 1, 2, 3, 4, 3, 4, 5], 3) == 4
+    assert search_in_adjacent_numbers([4, 5, 6, 5, 6, 7, 8, 9, 10, 9], 9) == 7
+
+
+def test_search_in_adjacent_numbers_not_in_list():
+    with pytest.raises(ValueError) as e:
+        search_in_adjacent_numbers([1, 2, 1], 3)
+    assert str(e.value) == "3 is not in list"
+
+    with pytest.raises(ValueError) as e:
+        search_in_adjacent_numbers([1, 2, 1], 6)
+    assert str(e.value) == "6 is not in list"
