a018e3de113d5c94bbae3530a680337b7633daad
Denis Costa <deniscostadsc@gmail.com>
2020-06-09 20:33:25 -0300

    Fix lint

--- src/problems/coder_career/search_in_adjacent_numbers.py
+++ src/problems/coder_career/search_in_adjacent_numbers.py
@@ -1,5 +1,6 @@
 from typing import List
 
+
 """
 Given an array where two neighboring elements are adjacent (in absolute
 difference 1), can you write an algorithm to search an element in the array and
