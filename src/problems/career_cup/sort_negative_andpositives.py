0d5c81afbb26a0e63410c0f46232384de072ac20
Denis Costa <deniscostadsc@gmail.com>
2020-06-13 20:02:54 -0300

    Add unsolved problem

---
+++ src/problems/career_cup/sort_negative_andpositives.py
@@ -0,0 +1,13 @@
+"""
+Give you an array which has n integers,it has both  positive and negative
+integers. Now you need sort this array in a special way. After that, the
+negative integers should be in the front, and the positive integers should in
+the back.Also the relative position should not be changed.
+
+
+eg. -1 1 3 -2 2 ans: -1 -2 1 3 2.
+
+o(n)time complexity and o(1) space complexity is perfect.
+
+https://www.careercup.com/question?id=5201559730257920
+"""
